a63010fe204df0fea54e635e88b30370f224e714
Francis <francisdesign27@gmail.com>
2018-03-20 13:41:22 -0430

    Evitar registros Duplicados

--- reg.php
+++ reg.php
@@ -1,12 +1,13 @@
 <?php
 //Conexion con la base de datos y el servidor
-//$link = mysqli_connect("localhost","root","") or die("<h2>No se encuentra el servidor</h2>");
 $link = mysqli_connect("localhost","root","", "usuarios") or die("<h2>No se encuentra el servidor</h2>");
+//$link = mysqli_connect("localhost","root","") or die("<h2>No se encuentra el servidor</h2>");
 //$db = mysqli_select_db("usuarios",$link) or die("<h2>Error del servidor</h2>");
+
 session_start();
 error_reporting(0);
 
-//obtenemos los valores del formulario
+//Obtenemos los valores del formulario
 $nombre = $_POST['nombre'];
 $usuario = $_POST['usuario'];
 $correo = $_POST['correo'];
@@ -21,7 +22,6 @@ if($varsesion == null || $varsesion = ''){
   header("location:autorizacion.html");
   die();
 }
-//header("location:login.html");
 
 //Obtener la longitud de un string
 $req = (strlen($nombre)*strlen($usuario)*strlen($correo)*strlen($pass)*strlen($rpass)) or die("No se han llenado todos los campos");
@@ -34,17 +34,48 @@ if($pass != $rpass){
 //Se encripta la contraseña
 $pass = md5($pass);
 
-//ingresar la informacion a la tabla datos
-mysqli_query($link, "INSERT INTO datos (nombre_completo,usuario,correo,password)
-VALUES ('$nombre','$usuario','$correo','$pass')") or die("<h2>Error de envio</h2>");
+//Consulta para insertar
+$insertar = "INSERT INTO datos (nombre_completo,usuario,correo,password) VALUES ('$nombre','$usuario','$correo','$pass')";
+
+//Evitar registros Duplicados
+$verificar_usuario = mysqli_query($link, "SELECT * FROM datos WHERE usuario = '$usuario'");
+if (mysqli_num_rows($verificar_usuario) > 0){
+  echo '
+    <script>
+      alert("El usuario ya esta registrado");
+      window.history.go(-1);
+    </script>
+  ';
+  die();
+}
+$verificar_correo = mysqli_query($link, "SELECT * FROM datos WHERE correo = '$correo'");
+if (mysqli_num_rows($verificar_correo) > 0){
+  echo '
+    <script>
+      alert("El correo ya esta registrado");
+      window.history.go(-1);
+    </script>
+  ';
+  die();
+}
+
+//Ejecutar consulta
+$resultado = mysqli_query($link, $insertar);
 //mysqli_query($link, "INSERT INTO datos VALUES ('','$nombre','$usuario','$correo','$pass')") or die("<h2>Error de envio</h2>");
-
 //mysqli_query("INSERT INTO datos VALUES ('','$nombre','$usuario','$correo','$pass')",$link) or die("<h2>Error de envio</h2>");
+if (!$resultado){
+   echo "
+   <h2>Error de envio</h2>
+   ";
+}
+else {
+  echo "
+  <h2>Registro Completado</h2>
+  <h5>Gracias por registrarse en nuestra web, ya puede ingresar como usuario</h5>
+  <a href='login2.php' >Iniciar Sesion</a>
+  ";
+}
 
-echo "
-<h2>Registro Completado</h2>
-<h5>Gracias por registrarse en nuestra web, ya puede ingresar como usuario</h5>
-<a href='login2.php' >Iniciar Sesion</a>
-";
-mysqli_close($conexion);
+//Cerrar Conexion
+mysqli_close($link);
  ?>
